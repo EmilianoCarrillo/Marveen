404a5cf40fccc1f646c3211135e77bb3c58b962f
omargard99 <38355029+omargard99@users.noreply.github.com>
2018-04-14 03:55:38 -0500

    Conecction API
    Twitter conecction

--- public/marveen.php
+++ public/marveen.php
@@ -2,20 +2,18 @@
 	require_once('TwitterAPIExchange.php');
 
 	$settings = array(
-    'oauth_access_token' => "",
+    'oauth_access_token' => "860092922657619969-O75q6QXhZZUcIygfEMHFa2E5zd2IGSP",
     'oauth_access_token_secret' => "e7BqhLAmxzl2fek9KYeNzpKpPkAOOt7uXURlcwP4cGiGe",
-    'consumer_key' => "860092922657619969-O75q6QXhZZUcIygfEMHFa2E5zd2IGSP",
+    'consumer_key' => "HhHKXbCf940bmiJyIABPfQnmc",
     'consumer_secret' => "ptbPNPWJgMg9SmvTxsTsbeRyC8DqKIHL1g3NGqs5WTGsXRM5OU"
 	);
 	$url = 'https://api.twitter.com/1.1/search/tweets.json';
-	$getfield = '?q=#EsposaEmbarazada';
+	$getfield = '?q=Embarazada';
 	$requestMethod = 'GET';
 
 	$twitter = new TwitterAPIExchange($settings);
 	$response = $twitter->setGetfield($getfield)
 	    ->buildOauth($url, $requestMethod)
 	    ->performRequest();
-
 	var_dump(json_decode($response));
-	
 ?>
