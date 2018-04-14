26e2920b2c0ac47950c7932ec401111f32650f24
omargard99 <38355029+omargard99@users.noreply.github.com>
2018-04-14 03:11:00 -0500

    Php Twitter API try
    Php Twitter API try

---
+++ public/marveen.php
@@ -0,0 +1,21 @@
+<?php
+	require_once('TwitterAPIExchange.php');
+
+	$settings = array(
+    'oauth_access_token' => "",
+    'oauth_access_token_secret' => "e7BqhLAmxzl2fek9KYeNzpKpPkAOOt7uXURlcwP4cGiGe",
+    'consumer_key' => "860092922657619969-O75q6QXhZZUcIygfEMHFa2E5zd2IGSP",
+    'consumer_secret' => "ptbPNPWJgMg9SmvTxsTsbeRyC8DqKIHL1g3NGqs5WTGsXRM5OU"
+	);
+	$url = 'https://api.twitter.com/1.1/search/tweets.json';
+	$getfield = '?q=#EsposaEmbarazada';
+	$requestMethod = 'GET';
+
+	$twitter = new TwitterAPIExchange($settings);
+	$response = $twitter->setGetfield($getfield)
+	    ->buildOauth($url, $requestMethod)
+	    ->performRequest();
+
+	var_dump(json_decode($response));
+	
+?>
